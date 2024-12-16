181b5391dc1ad5ba108981e69f7927ef236952da
jorguitart <jorgejfpx@gmail.com>
2024-12-16 13:02:40 +0100

    New changes

--- project/main_script.R
+++ project/main_script.R
@@ -1,3 +1,7 @@
 ####----LOAD LIBRARIES----####
 
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
 library(Seurat)
+library(spacexr) #devtools::install_github("dmcable/spacexr", build_vignettes = FALSE)

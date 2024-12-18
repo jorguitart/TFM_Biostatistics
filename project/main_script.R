44caa6f67095e76ca268504b494ca136f6469ea8
jorguitart <jorgejfpx@gmail.com>
2024-12-18 12:25:53 +0100

    Added new libraries

--- project/main_script.R
+++ project/main_script.R
@@ -6,3 +6,6 @@ library(ggplot2)
 library(Seurat)
 library(spacexr) #devtools::install_github("dmcable/spacexr", build_vignettes = FALSE)
 library(nicheDE) #devtools::install_github("kaishumason/NicheDE")
+library(spatialGE) #devtools::install_github("fridleylab/spatialGE")
+
+

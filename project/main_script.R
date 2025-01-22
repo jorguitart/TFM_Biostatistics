88d4236ad2adbd33f9bc45b406c762ebdf3575a0
jorguitart <jorgejfpx@gmail.com>
2025-01-22 14:11:12 +0100

    No sé qué pasa

--- project/main_script.R
+++
@@ -1,8 +0,0 @@
-####----LOAD LIBRARIES----####
-
-library(dplyr)
-library(tidyverse)
-library(ggplot2)
-library(Seurat)
-library(spacexr) #devtools::install_github("dmcable/spacexr", build_vignettes = FALSE)
-library(nicheDE) #devtools::install_github("kaishumason/NicheDE")

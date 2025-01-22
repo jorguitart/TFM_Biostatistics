e9ba0422875e4cfbb8c690db2859892b79b78f55
jorguitart <jorgejfpx@gmail.com>
2025-01-22 14:38:07 +0100

    Added code

---
+++ project/main_script.R
@@ -0,0 +1,13 @@
+####----LOAD LIBRARIES----####
+
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(Seurat)
+library(spacexr) #devtools::install_github("dmcable/spacexr", build_vignettes = FALSE)
+library(nicheDE) #devtools::install_github("kaishumason/NicheDE")
+library(spatialGE) #devtools::install_github("fridleylab/spatialGE")
+library(Giotto) #remotes::install_github("/Giotto@main")
+
+####----MAIN CODE----####
+

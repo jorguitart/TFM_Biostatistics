46f354463b29991a466af411070f8bcdbc544a20
jorguitart <jorgejfpx@gmail.com>
2025-01-22 14:40:36 +0100

    Changes added

--- project/main_script.R
+++ project/main_script.R
@@ -7,7 +7,7 @@ library(Seurat)
 library(spacexr) #devtools::install_github("dmcable/spacexr", build_vignettes = FALSE)
 library(nicheDE) #devtools::install_github("kaishumason/NicheDE")
 library(spatialGE) #devtools::install_github("fridleylab/spatialGE")
-library(Giotto) #remotes::install_github("/Giotto@main")
+library(Giotto) #remotes::install_github("drieslab/Giotto@main")
 
 ####----MAIN CODE----####
 

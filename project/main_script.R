5c7df3664ab8598f42f4e27a1316a6e78a026a60
jorguitart <jorgejfpx@gmail.com>
2025-01-27 20:41:14 +0100

    Changed code

--- project/main_script.R
+++ project/main_script.R
@@ -6,7 +6,7 @@ library(ggplot2)
 library(spacexr) #devtools::install_github("dmcable/spacexr", build_vignettes = FALSE)
 library(nicheDE) #devtools::install_github("kaishumason/NicheDE")
 library(spatialGE) #devtools::install_github("fridleylab/spatialGE")
-library(Giotto) #remotes::install_github("drieslab/Giotto@master")
+library(Giotto) #pak::pkg_install("drieslab/Giotto")
 library(scran)
 
 ####--DIRECTORIES--####
@@ -19,34 +19,29 @@ instr <- createGiottoInstructions(save_dir = "./project/outcomes",
 # Create sample dirs
 dir <- "./project/material/GSE165098"
 sample.ids <- c("LPS1", "LPS2", "LPS3", "saline1", "saline2", "saline3")
-h5.path <- file.path(dir, sample.ids, "filtered_feature_bc_matrix.h5")
-pos.path <- file.path(dir, sample.ids, "spatial/tissue_positions_list.csv")
-png.path <- file.path(dir, sample.ids, "spatial/tissue_lowres_image.png")
-
+sample.path <- file.path(dir, sample.ids)
 
 ####--SAMPLE 1--####
 
 # Obtain data
-s1 <- createGiottoVisiumObject(h5_visium_path = h5.path[1], 
-                               h5_tissue_positions_path = pos.path[1], 
-                               h5_image_png_path = png.path[1], 
-                               instructions = instr) #LPS1
+s1 <- createGiottoVisiumObject(visium_dir = sample.path[1], # LPS1
+                               expr_data = "filter",
+                               png_name = "tissue_lowres_image.png") # Not well aligned
 
 # Visualize image
-s1 <- updateGiottoImage(s1, image_name = "image", # Aligning
-                        xmax_adj = 690, xmin_adj = 690,
-                        ymax_adj = 820, ymin_adj = 520)
-
-spatPlot(s1, show_image = T, point_alpha = 0.6)
+spatPlot(s1, point_alpha = 0.6)
 
 # Quality control 
 ## Select over tissue genes
-in.tissue <- pDataDT(s1)[in_tissue == 1]$cell_ID
-s1 <- subsetGiotto(s1, cell_ids = in.tissue) 
-s1@parameters$"0_subset" # No genes/cells deleted
+
+if (sum((in.tissue == pDataDT(s1)$cell_ID) == F) == 0) {
+  cat("All genes are over tissue")
+} else {
+  s1 <- subsetGiotto(s1, cell_ids = in.tissue)
+  }
 
 ## Identify mitochondrial genes
-is_mito <- grepl("(^MT-)|(^mt-)", s1@gene_ID)
+is_mito <- grepl("(^MT-)|(^mt-)", s1@feat_ID)
 if (sum(is_mito == T) != 0) {
   cat(sum(is_mito == T), "mitochondrial genes found:", which(is_mito == T))
   } else {
@@ -54,8 +49,8 @@ if (sum(is_mito == T) != 0) {
     } # 13 mitochondrial genes
 
 ## Histograms
-filterDistributions(s1, detection = "cells", nr_bins = 100) # Total genes per spot (threshold = 0)
-filterDistributions(s1, detection = "genes", nr_bins = 100) # Genes detected per amount of spots
+filterDistributions(s1, detection = "genes", nr_bins = 100) # Genes detected per spot (threshold = 0)
+
 
 ## Tresholds
 filterCombinations(s1)

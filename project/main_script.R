f3ac1870953f686b60aa2f5edea6c7ca17f6f256
jorguitart <jorgejfpx@gmail.com>
2025-01-27 22:03:38 +0100

    Updated code

--- project/main_script.R
+++ project/main_script.R
@@ -1,4 +1,4 @@
-####----LOAD LIBRARIES----####
+####--LIBRARIES--####
 
 library(dplyr)
 library(tidyverse)
@@ -7,13 +7,12 @@ library(spacexr) #devtools::install_github("dmcable/spacexr", build_vignettes = 
 library(nicheDE) #devtools::install_github("kaishumason/NicheDE")
 library(spatialGE) #devtools::install_github("fridleylab/spatialGE")
 library(Giotto) #pak::pkg_install("drieslab/Giotto")
-library(scran)
 
 ####--DIRECTORIES--####
 
 # Create save instructions
 instr <- createGiottoInstructions(save_dir = "./project/outcomes",
-                                  save_plot = TRUE)
+                                  save_plot = T, show_plot = F)
 
 
 # Create sample dirs
@@ -24,33 +23,53 @@ sample.path <- file.path(dir, sample.ids)
 ####--SAMPLE 1--####
 
 # Obtain data
-s1 <- createGiottoVisiumObject(visium_dir = sample.path[1], # LPS1
-                               expr_data = "filter",
-                               png_name = "tissue_lowres_image.png") # Not well aligned
+s1 <- createGiottoVisiumObject(visium_dir = sample.path[1], #LPS1
+                               gene_column_index = 2, # Use gene symbols
+                               expr_data = "filter", # Use filtered data
+                               png_name = "tissue_lowres_image.png", # Lowres
+                               instructions = instr)
 
-# Visualize image
-spatPlot(s1, point_alpha = 0.6)
+# Visualize spots
+s1.spots <- spatPlot(s1, point_alpha = 0.6, 
+                     default_save_name = "spots") # Image not properly aligned
+s1.spots
 
 # Quality control 
 ## Select over tissue genes
 in.tissue <- pDataDT(s1)[in_tissue == 1]$cell_ID
+
 if (sum((in.tissue == pDataDT(s1)$cell_ID) == F) == 0) {
   cat("All genes are over tissue")
+  rm(in.tissue)
 } else {
   s1 <- subsetGiotto(s1, cell_ids = in.tissue)
-  }
+  rm(in.tissue)
+  } # All genes are over tissue
 
 ## Identify mitochondrial genes
-is_mito <- grepl("(^MT-)|(^mt-)", s1@feat_ID)
+is_mito <- grepl("(^MT-)|(^mt-)", s1@feat_ID$rna)
 if (sum(is_mito == T) != 0) {
   cat(sum(is_mito == T), "mitochondrial genes found:", which(is_mito == T))
+  rm(is_mito)
   } else {
     cat('No mitochondrial genes found')
-    } # 13 mitochondrial genes
+    rm(is_mito)
+    } # 13 mitochondrial genes found
 
 ## Histograms
-filterDistributions(s1, detection = "cells", nr_bins = 100) # Genes detected per spot (threshold = 0)
+### Detected genes per cell
+s1.detected <- filterDistributions(s1, detection = "cells", nr_bins = 150,
+                                   method = "threshold",
+                                   default_save_name = "detected_genes")
+s1.detected
 
+### Library size
+s1.libsize <- filterDistributions(s1, detection = "cells", nr_bins = 150,
+                                  method = "sum",
+                                  default_save_name = "library_size")
+s1.libsize
 
 ## Tresholds
-filterCombinations(s1)
+s1.thresholds <- filterCombinations(s1, show_plot = F, 
+                                    default_save_name = "thresholds")
+s1.thresholds

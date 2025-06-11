6f84799f17498d11f552528522daa255e854c616
jorguitart <jorgejfpx@gmail.com>
2025-06-11 16:04:39 +0200

    .

--- project/visualization.R
+++ project/visualization.R
@@ -8,9 +8,10 @@ library(Giotto) #pak::pkg_install("drieslab/Giotto")
 
 
 ####--DATA--####
-load(file = "./project/material/postHMRF.RData")
+sample <- loadGiotto(path_to_folder = "./project/material/filtered_samples/resolved_sample", 
+                     python_path = "C:/ProgramData/anaconda3/python.exe")
 sample@spatial_info <- sample@spatial_locs
 
-spatPlot2D(sample, cell_color = "leiden_clus", show_plot = T)
+spatPlot2D(sample, group_by = "list_ID", cell_color = "leiden_clus", show_plot = T)
 
 

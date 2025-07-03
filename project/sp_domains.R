d1bd352783e2729dc1f8e80196ee1a6d333259ce
jorguitart <jorgejfpx@gmail.com>
2025-07-03 10:31:47 +0200

    .

--- project/sp_domains.R
+++ project/sp_domains.R
@@ -6,6 +6,7 @@ library(Giotto) #pak::pkg_install("drieslab/Giotto")
 # library(nicheDE) #devtools::install_github("kaishumason/NicheDE")
 # library(spatialGE) #devtools::install_github("fridleylab/spatialGE")
 
+setwd("~/TFM")
 
 ####--HMRF--####
 t0 <- Sys.time()
@@ -14,24 +15,19 @@ if(!file.exists("./project/material/preHMRF.RData")) {
   
   message("Loading sample...")
   sample <- loadGiotto(path_to_folder = "./project/material/filtered_samples/normalized_sample", 
-                       python_path = "C:/ProgramData/anaconda3/python.exe")
+                       python_path = "/usr/bin/python36")
   
   message("Creating spatial network...")
-  sample <- createSpatialNetwork(sample, minimum_k = 6, name = "spat_network", method = "Delaunay", maximum_distance_delaunay = 40) 
+  sample <- createSpatialDelaunayNetwork(sample, name = "spat_network")
   
   message("Extracting spatial genes...")
-  sample <- binSpect(sample, expression_values = "normalized", bin_method = "kmeans", spatial_network_name = "spat_network", return_gobject = T)
+  sample <- binSpect(sample, expression_values = "normalized", bin_method = "kmeans", spatial_network_name = "spat_network")
   sp.feats <- sample@feat_metadata$cell$rna$feat_ID[order(sample@feat_metadata$cell$rna$binSpect.pval)][1:500]
   
-  message("Calculating spatial correlation genes...")
-  sp.cor <- detectSpatialCorFeats(sample, method = "network", spatial_network_name = "spat_network", subset_feats = sp.feats)
-  sp.cor <- clusterSpatialCorFeats(sp.cor, name = "spnetwork_cl", k = 13)
-  cl.feats <- showSpatialCorFeats(sp.cor, use_clus_name = "spnetwork_cl", show_top_feats = 1)
-  
   message("Creating HMRF object...")
-  sample@dimension_reduction$cells$cell$rna$spatial$spatial_feat <- sample@dimension_reduction$cells$cell$rna$pca$pca
+  # sample@dimension_reduction$cells$cell$rna$spatial$spatial_feat <- sample@dimension_reduction$cells$cell$rna$pca$pca
   saveGiotto(sample, foldername = "preinit_sample", dir = "./project/material/filtered_samples", overwrite = T)
-  sample.hmrf <- initHMRF_V2(sample, spat_unit = "cell", feat_type = "rna", cl.method = "leiden", user_gene_list = sp.feats, 
+  sample.hmrf <- initHMRF_V2(sample, cl.method = "leiden", user_gene_list = sp.feats, 
                              spatial_network_name = "spat_network", k = 7, resolution.cl = 0.25)
   
   message("Saving environment image...")

8b2f23d6be60add677321b6ae245e185aab2297c
jorguitart <jorgejfpx@gmail.com>
2025-07-19 13:38:29 +0200

    .

--- project/scripts/spatialGE/newsGEobject.R
+++ project/scripts/spatialGE/newsGEobject.R
@@ -12,12 +12,18 @@ setwd("~/TFM")
 sample <- loadGiotto("./project/material/filtered_samples/resolved_sample",
                      python_path = "C:/ProgramData/anaconda3/python.exe")
 
-rnacounts <- as.data.frame(as.matrix(sample@expression$cell$rna$normalized@exprMat))
-spotcoords <- sample@spatial_locs$cell$raw@coordinates
+rnacounts <- as.data.frame(as.matrix(sample@expression$cell$rna$raw@exprMat))
+counts <- rnacounts
+counts <- data.frame(genes = rownames(counts), counts)
+names.counts <- gsub("\\.", "-", colnames(counts)[-1])
+colnames(counts) <- c("genes", names.counts)
 
+spotcoords <- as.data.frame(sample@spatial_locs$cell$raw@coordinates)
+spotcoords <- spotcoords[, c(3, 1, 2)]; rownames(spotcoords) <- spotcoords$cell_ID
+spotcoords <- spotcoords[colnames(counts[, -1]), ]
 
-
-rna <- list(list(rnacounts)); names(rna) <- "sample"
-spot <- list(list(spotcoords)); names(spot) <- "sample"
+rna <- list(sample = counts)
+spot <- list(sample = spotcoords)
 
 sGE.obj <- STlist(rnacounts = rna, spotcoords = spot)
+save(sGE.obj, file = "./project/material/spatialGE/newsGEobject.RData")

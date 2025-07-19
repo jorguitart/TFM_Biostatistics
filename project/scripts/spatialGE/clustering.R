799b9f72877fdd0d5f94fa1c7c40c517900a2889
jorguitart <jorgejfpx@gmail.com>
2025-07-19 13:47:30 +0200

    .

--- project/scripts/spatialGE/clustering.R
+++ project/scripts/spatialGE/clustering.R
@@ -11,5 +11,6 @@ setwd("~/TFM")
 ####--DATA--####
 load("./project/material/spatialGE/newsGEobject.RData")
 
+sGE.obj <- transform_data(sGE.obj, scale_f = 6000)
 sGE.obj <- STclust(sGE.obj, ws = 0.5, topgenes = 500, cores = 10)
 save(sGE.obj, file = "./project/material/spatialGE/sGEclust.RData")

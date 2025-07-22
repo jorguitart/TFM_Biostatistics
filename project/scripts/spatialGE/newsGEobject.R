a978b8ecdd97fa39f4d9324a10cbf922e0465b3d
jorguitart <jorgejfpx@gmail.com>
2025-07-22 12:04:47 +0200

    .

--- project/scripts/spatialGE/newsGEobject.R
+++ project/scripts/spatialGE/newsGEobject.R
@@ -1,7 +1,7 @@
 ####--LIBRARIES--####
 library(tidyverse)
 library(ggpubr)
-# library(Giotto) #pak::pkg_install("drieslab/Giotto")
+library(Giotto) #pak::pkg_install("drieslab/Giotto")
 # library(spacexr) #devtools::install_github("dmcable/spacexr", build_vignettes = FALSE)
 # library(nicheDE) #devtools::install_github("kaishumason/NicheDE")
 library(spatialGE) #devtools::install_github("fridleylab/spatialGE")
@@ -9,13 +9,51 @@ library(spatialGE) #devtools::install_github("fridleylab/spatialGE")
 setwd("~/TFM")
 
 ####--DATA--####
-dir <- "./project/material/GSE279181"
-sam <- c("CO37", "CO40", "CO41", "CO74", "CO85", "CO96", 
-         "MS197D", "MS197U", "MS377N", "MS377T", "MS377I", "MS411", 
-         "MS497I", "MS497T", "MS549H", "MS549T")
-sample.path <- file.path(dir, sam)
+# Create STlist
+sample <- loadGiotto("./project/material/Giotto/HMRF_sample")
 
-sGE.obj <- STlist(rnacounts = sample.path, samples = sam)
-sGE.obj <- filter_data(sGE.obj, spot_mingenes = 125, gene_minspots = 28)
-sGE.obj <- transform_data(sGE.obj, scale_f = 6000)
+samples <- list(); rnacounts <- list(); names.counts <- list(); spotcoords <- list()
+
+for (i in unique(sample@cell_metadata$cell$rna$list_ID)) {
+  samples[[i]] <- subsetGiotto(sample, cell_ids = pDataDT(sample)[list_ID %in% i]$cell_ID)
+
+  rnacounts[[i]] <- as.data.frame(as.matrix(samples[[i]]@expression$cell$rna$raw@exprMat))
+  rnacounts[[i]] <- data.frame(genes = rownames(rnacounts[[i]]), rnacounts[[i]])
+  names.counts[[i]] <- gsub("\\.", "-", colnames(rnacounts[[i]])[-1])
+  colnames(rnacounts[[i]]) <- c("genes", names.counts[[i]])
+
+  spotcoords[[i]] <- as.data.frame(samples[[i]]@spatial_locs$cell$raw@coordinates)
+  spotcoords[[i]] <- spotcoords[[i]][, c(3, 2, 1)]
+  spotcoords[[i]] <- spotcoords[[i]][spotcoords[[i]]$cell_ID == colnames(rnacounts[[i]][, -1]), ]
+}
+
+newsGE.obj <- STlist(rnacounts = rnacounts, spotcoords = spotcoords)
+
+# Add metadata
+load("./project/material/spatialGE/sGEclust.RData")
+
+## Normalized counts
+tr_counts <- list()
+for (i in unique(sample@cell_metadata$cell$rna$list_ID)) {
+  tr_counts[[i]] <- sGE.obj@tr_counts$sample[, grep(i, sGE.obj@tr_counts$sample@Dimnames[[2]])]
+}
+newsGE.obj@tr_counts <- tr_counts
+
+## Domains
+sp.meta <- sGE.obj@spatial_meta$sample
+names(sp.meta)[names(sp.meta) == "stclust_spw0.5_dsplFalse"] <- "cluster"
+
+clusters <- list()
+for (i in unique(sample@cell_metadata$cell$rna$list_ID)) {
+  clusters[[i]] <- sp.meta$cluster[grep(i, sp.meta$libname)]
+}
+
+for (i in unique(sample@cell_metadata$cell$rna$list_ID)) {
+  newsGE.obj@spatial_meta[[i]][["cluster"]] <- clusters[[i]]
+}
+
+## Sample type
+sample_type <- c(rep("CTRL", 5), rep("MSCA", 4), rep("MSCI", 3))
+newsGE.obj@sample_meta$sample_type <- sample_type
+
 save(sGE.obj, file = "./project/material/spatialGE/newsGEobject.RData")

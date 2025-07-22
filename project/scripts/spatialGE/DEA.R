9c661e55109ad190efa8ab457cc647c23ecdbade
jorguitart <jorgejfpx@gmail.com>
2025-07-22 19:44:33 +0200

    .

---
+++ project/scripts/spatialGE/DEA.R
@@ -0,0 +1,17 @@
+####--LIBRARIES--####
+library(tidyverse)
+library(ggpubr)
+# library(Giotto) #pak::pkg_install("drieslab/Giotto")
+# library(spacexr) #devtools::install_github("dmcable/spacexr", build_vignettes = FALSE)
+# library(nicheDE) #devtools::install_github("kaishumason/NicheDE")
+library(spatialGE) #devtools::install_github("fridleylab/spatialGE")
+
+#setwd("~/TFM")
+
+####--DATA--####
+load("./project/material/spatialGE/newsGEobject.RData")
+
+DEA <- STdiff(newsGE.obj, samples = c("MS377N", "MS549H"), annot = "domain", clusters = c("LC", "LR"),
+              topgenes = 500, test_type = "t_test", cores = 10)
+
+save(DEA, file = "./project/material/spatialGE/DEA.RData")
